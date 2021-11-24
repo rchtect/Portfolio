35f25d0a7d02ed36a1a963acb0a16444b48efe34
Isaaq Guleed <st0lenstar@icloud.com>
2021-11-24 22:11:02 +0100

    Initial commit

---
+++ public/script/darkmode.js
@@ -0,0 +1,60 @@
+var darkColor = "#121212"
+function isDark() {
+  document.body.style.transition = "0ms"
+  var values = window.localStorage.getItem('value')
+  if (values === null || values.length === 0)
+  {
+    document.getElementById("light-logo").style.display = "none";
+    document.getElementById("dark-logo").style.display = "block";
+    document.documentElement.style.setProperty('--toggle-color', 'black');
+    document.getElementById("btn-1").style.display = "block";
+    document.getElementById("btn-2").style.display = "none";
+    document.body.style.backgroundColor = "white";
+    
+  }
+  if (localStorage.getItem("value") == "true") {
+    document.getElementById("light-logo").style.display = "block";
+    document.getElementById("dark-logo").style.display = "none";
+    document.documentElement.style.setProperty('--toggle-color', 'white');
+    document.getElementById("btn-1").style.display = "none";
+    document.getElementById("btn-2").style.display = "block";
+    document.body.style.backgroundColor = darkColor;
+    
+  } else if (localStorage.getItem("value") == "false"){
+    document.getElementById("light-logo").style.display = "none";
+    document.getElementById("dark-logo").style.display = "block";
+    document.documentElement.style.setProperty('--toggle-color', 'black');
+    document.getElementById("btn-1").style.display = "block";
+    document.getElementById("btn-2").style.display = "none";
+    document.body.style.backgroundColor = "white";
+    
+  }
+}
+
+function toggleDark() {
+  document.getElementById("light-logo").style.display = "block";
+  document.getElementById("dark-logo").style.display = "none";
+  document.documentElement.style.setProperty('--toggle-color', 'white');
+  document.getElementById("btn-1").style.display = "none";
+  document.getElementById("btn-2").style.display = "block";
+  document.body.style.backgroundColor = darkColor;
+  localStorage.setItem("value", "true");
+  document.body.style.transition = "150ms"
+}
+
+function toggleLight() {
+  document.documentElement.style.setProperty('--toggle-color', 'black');
+  document.getElementById("light-logo").style.display = "none";
+  document.getElementById("dark-logo").style.display = "block";
+  document.getElementById("btn-1").style.display = "block";
+  document.getElementById("btn-2").style.display = "none";
+  document.body.style.backgroundColor = "white";
+  localStorage.setItem("value", "false");
+  document.body.style.transition = "150ms"
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  isDark();
+  document.getElementById("btn-1").addEventListener("click", toggleDark);
+  document.getElementById("btn-2").addEventListener("click", toggleLight);
+});
